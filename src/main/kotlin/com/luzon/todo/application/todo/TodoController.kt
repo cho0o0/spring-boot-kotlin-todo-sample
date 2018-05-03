65cff20f0a2b79f880535f7d2ddbd72c274faa8f
cho0o0 <choqu@outlook.com>
2018-05-03 14:27:55 +0900

    orElseGet -> orElse

--- src/main/kotlin/com/luzon/todo/application/todo/TodoController.kt
+++ src/main/kotlin/com/luzon/todo/application/todo/TodoController.kt
@@ -16,11 +16,11 @@ class CustomerController(private val repository: ITodoRepository) {
     fun findAll(): List<Todo> = repository.findAll()
 
     @GetMapping("/{id}")
-    fun findByLastName(@PathVariable id: TodoId) : Todo? = repository.findById(id).orElseGet(null)
+    fun findByLastName(@PathVariable id: TodoId) : Todo? = repository.findById(id).orElse(null)
 
     @PatchMapping("/{id}/done")
     fun done(@PathVariable id: TodoId) : Todo? {
         val todo = repository.findById(id)
-        return todo.map({ it.done() }).orElseGet(null)
+        return todo.map({ it.done() }).orElse(null)
     }
 }
