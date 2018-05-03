1a2923b8c0545b9aeeda4e46ea13fba8a513969c
cho0o0 <choqu@outlook.com>
2018-05-03 21:50:15 +0900

    improve format

--- src/main/kotlin/com/luzon/todo/application/todo/TodoAppService.kt
+++ src/main/kotlin/com/luzon/todo/application/todo/TodoAppService.kt
@@ -6,6 +6,7 @@ import com.luzon.todo.domain.todo.TodoId
 import com.luzon.todo.domain.todo.TodoSpecification
 import com.luzon.todo.infrastructure.IIdGeneratorService
 import org.springframework.stereotype.Service
+import java.util.stream.Stream
 
 @Service
 class TodoAppService(
@@ -13,40 +14,33 @@ class TodoAppService(
         private val idGeneratorService: IIdGeneratorService,
         private val todoSpecification: TodoSpecification
 ) {
-    fun create(todoDto: TodoDTO): TodoDTO {
-        return convertToTodoDTO(repository.save(convertToTodo(todoDto)))
-    }
+    fun create(todoDto: TodoDTO): TodoDTO =
+            Stream.of(todoDto)
+                    .map(this::convertToTodo)
+                    .map { todo -> repository.save(todo) }
+                    .map(this::convertToTodoDTO)
+                    .findAny()
+                    .get()
 
-    fun findAllTodos(): List<TodoDTO> {
-        return convertToTodoDTOs(repository.findAll())
-    }
+    fun findAllTodos(): List<TodoDTO> = convertToTodoDTOs(repository.findAll())
 
-    fun findByTodoId(todoId: TodoId): TodoDTO? {
-        return repository.findOne(todoSpecification.id(todoId))
-                .map(this::convertToTodoDTO)
-                .orElse(null)
-    }
+    fun findByTodoId(todoId: TodoId): TodoDTO? =
+            repository.findOne(todoSpecification.id(todoId))
+                    .map(this::convertToTodoDTO)
+                    .orElse(null)
 
-    fun completeTodoById(todoId: TodoId): TodoDTO? {
-        return repository.findOne(todoSpecification.id(todoId))
-                .map({ it.done() })
-                .map({ todo -> repository.save(todo) })
-                .map(this::convertToTodoDTO)
-                .orElse(null)
-    }
+    fun completeTodoById(todoId: TodoId): TodoDTO? = repository.findOne(todoSpecification.id(todoId))
+            .map { it.done() }
+            .map { todo -> repository.save(todo) }
+            .map(this::convertToTodoDTO)
+            .orElse(null)
 
-    private fun convertToTodo(todoDto: TodoDTO): Todo {
-        return Todo(idGeneratorService.generate(), todoDto.message)
-    }
+    private fun convertToTodo(todoDto: TodoDTO): Todo = Todo(idGeneratorService.generate(), todoDto.message)
 
-    private fun convertToTodoDTO(todo: Todo): TodoDTO {
-        return TodoDTO(
-                todo.id, todo.message, todo.createdDate, todo.updatedDate, todo.isDone
-        )
-    }
+    private fun convertToTodoDTO(todo: Todo): TodoDTO = TodoDTO(
+            todo.id, todo.message, todo.createdDate, todo.updatedDate, todo.isDone
+    )
 
-    private fun convertToTodoDTOs(todos: List<Todo>): List<TodoDTO> {
-        return todos.map { convertToTodoDTO(it) }
-    }
+    private fun convertToTodoDTOs(todos: List<Todo>): List<TodoDTO> = todos.map { convertToTodoDTO(it) }
 
 }
