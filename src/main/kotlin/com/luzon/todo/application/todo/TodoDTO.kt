c2339ac6b0a8456a64a39fec6493b3a8ffab13a9
cho0o0 <choqu@outlook.com>
2018-06-10 16:49:40 +0900

    rename graphql schema files

--- src/main/kotlin/com/luzon/todo/application/todo/TodoDTO.kt
+++ src/main/kotlin/com/luzon/todo/application/todo/TodoDTO.kt
@@ -4,7 +4,7 @@ import com.luzon.todo.infrastructure.Id
 import java.time.LocalDateTime
 
 data class TodoDTO(
-        val id: Id,
+        val id: Id?,
         val message: String,
         val createdDate: LocalDateTime?,
         val updatedDate: LocalDateTime?,
